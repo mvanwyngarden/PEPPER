29f75ec90005afcc97f6445198af6a7632a1f314
Madi VanWyngarden <mvanwyngarden02@gmail.com>
2022-06-24 10:17:59 -0400

    added and tested functions to calculate the density at the rcb and find the envelope mass fraction

--- planetstructure.py
+++ planetstructure.py
@@ -2,7 +2,9 @@
 
 import numpy as np
 from scipy import interpolate
+from scipy.integrate import quad
 from bisect import bisect_left
+import constants as cs
 import pdb
 
 def weights(Xiron):
@@ -31,9 +33,9 @@ def tables():
     
     return radtable, masstable
     
-def Rcore_to_Mcore(Xiron, rcore):
+def Rcore_to_Mcore(planet):
     
-    pos, coeff1, coeff2 = weights(Xiron)
+    pos, coeff1, coeff2 = weights(planet.Xiron)
     radtable, masstable = tables()
     
     
@@ -50,7 +52,7 @@ def Rcore_to_Mcore(Xiron, rcore):
         
     #use linear interpolation to find the mass value for a given radius value as a function of Xiron
     massfunc = interpolate.interp1d(radii, massarr, bounds_error=False)
-    mass = massfunc(rcore)
+    mass = massfunc(planet.radius)
     return mass
     
 
@@ -77,11 +79,104 @@ def Mcore_to_Rcore(Xiron, mcore):
     
     return radius
     
-def evaluate_X(Delta_Rrcb, Teq, Mcore, Tkh_Myr, Xiron):
-    #solve for mass envelope frac using the radius of the radiative convect boundary
+##Envelope structure 
+
+def integrand1(x, gamma):
+
+    return x*(1./x-1.)**(1./(gamma-1.))
+
+def integrand2(x, gamma):
+
+    return x**2.*(1./x-1.)**(1./(gamma-1.))
+
+#integrates eq 5 in Owen & Wu from Rc/Rrcb to 1 
+
+def get_I1(system, Rrcb, gamma):
     
-    #for density of core should I use planet.radius or core mass to core radius function? 
-    rho_core=planet
+    Rcore = cs.Rearth2cm(system.planetRocky.radius)
+    I1 = quad(integrand1, Rcore/Rrcb, 1, args=gamma)[0]
+    return I1
+
+def get_I2(system, Rrcb, gamma):
+    
+    Rcore = cs.Rearth2cm(system.planetRocky.radius)
+    I2 = quad(integrand2, Rcore/Rrcb, 1, args=gamma)[0]
+    return I2 
+
+def get_I2_I1(system, Rrcb, gamma):
+    
+    
+    ratio = get_I2(system, Rrcb, gamma)/get_I1(system, Rrcb, gamma)
+    return ratio 
+
+def calc_X(system, DR_rcb, Tkh_Myr, gamma): 
+    ''''
+    Calculates X given planet parameters'''
+    
+    Rcore = cs.Rearth2cm(system.planetRocky.radius) 
+    Rrcb = DR_rcb + Rcore
+    
+    #sound speed of atmosphere
+    c_atmos_sq = cs.kb * system.planetRocky.Teq / cs.mu
+    
+    adiabatic_grad = (gamma-1)/gamma
+    
+    #average core density 
+    rho_core = calc_coredensity(system)
+    
+    #using eq 13 from Owen & Wu 2017, factor out X
+    
+    rho_rcb_factoroutx = calc_rho_rcb(system, DR_rcb, 1, Tkh_Myr)
+    
+    
+    #X=Menv/Mcore
+    #by factoring out X from eq 13 we can replace the planet density in eq 4 with rho_rcb times x**(1+alpha)
+    #using eq 4 from Owen & Wu, divide out Mcore, resulting in an expression of Menv/Mcore 
+    Menv_Mcore = 3 * (Rrcb/Rcore)**3 * (rho_rcb_factoroutx/rho_core) * (adiabatic_grad *\
+                    (cs.G * cs.Mearth(system.planetRocky.Mcore)) / (c_atmos_sq * Rrcb))**(1/(gamma-1)) * get_I2(system, Rrcb, gamma)
+   
+    #because Menv/Mcore=X we can solve for X 
+    pdb.set_trace()
+    X = Menv_Mcore**(1/(1+(1/(1+cs.alpha))))
+    
+    return X
+    
+
+def calc_coredensity(system): 
+    #find average density of the core 
+    
+    rho_core_calc = cs.Mearth(system.planetRocky.Mcore)/ ( (4/3)*np.pi*(cs.Rearth2cm(system.planetRocky.radius))**3)
+   
+    return rho_core_calc
+
+
+
+def calc_rho_rcb(system, DR_rcb, X, Tkh_Myr): 
+    '''calculate the density at the rcb using your planet system and DR_rcb in cm'''
+    #within the function make all constants into cgs units
+    #then convert rho back into SI
+    
+    Rcore= cs.Rearth2cm(system.planetRocky.radius)
+    
+    #height of the rcb
+    #DR_rcb is in Earth radii
+    Rrcb = DR_rcb + Rcore
+
+    I2_I1 = get_I2_I1(system, Rrcb, cs.gamma)
+    
+    Tkh_sec= cs.Myr2sec(Tkh_Myr)
+    
+    
+    rho_rcb = (cs.mu/cs.kb) * (I2_I1 * 64 * np.pi * cs.sigma * system.planetRocky.Teq**(3-cs.alpha-cs.beta) * Rrcb * Tkh_sec / 
+                            (3 * cs.kappa0 * cs.Mearth(system.planetRocky.Mcore)* X))**(1/(1+cs.alpha))
+    
+    return rho_rcb
+ 
+#ok so I believe that this eq is correct
+#but calc_x is not
+    
+    
+    
     
 
 
