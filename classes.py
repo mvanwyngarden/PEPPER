387e5b2ec6f7ada5e1b5a7386e018e5afb5c6d14
Madi VanWyngarden <mvanwyngarden02@gmail.com>
2022-06-24 10:18:55 -0400

    added class for planet system

--- classes.py
+++ classes.py
@@ -7,6 +7,7 @@ This is a temporary script file.
 #planet structure
 import numpy as np
 import constants as cs 
+import planetstructure as ps 
 
 #create objects to hold planet data
 
@@ -26,33 +27,51 @@ class Star():
 
 class Planet():
     
-    def __init__(self,radius, period, star, radius_err=0, period_err=0, Xiron=1/3, albedo=0):
+    def __init__(self,radius, period, isrocky, radius_err=0, period_err=0, Xiron=1/3, albedo=0):
         self.radius = radius
         self.radius_err = radius_err
         self.period = period
         self.period_err = period_err
         self.Xiron = Xiron
         self.albedo=albedo
-        self.semimajor(star)
-        self.planettemp(star)
+        self.isrocky= isrocky
+        self.calcMcore()
+        
        
-    def semimajor(self, star):
-            self.a = ((( cs.G *star.mass*cs.Msun*(self.period*cs.d2s)**2)/(4*(np.pi)**2))**(1/3))/cs.Au2m
+    def calcsemimajor(self, star):
+            self.a = ((( cs.G *cs.Msun(star.mass)*(self.period*cs.d2s)**2)/(4*(np.pi)**2))**(1/3))/cs.Au2m
             
-    def planettemp(self, star):
-            self.Teq = (1-self.albedo)**(1/4)*star.Teff*np.sqrt(star.radius*cs.Rsun/(2*self.a*cs.Au2m))
+    def calcplanettemp(self, star):
+            self.Teq = (1-self.albedo)**(1/4)*star.Teff*np.sqrt(cs.Rsun(star.radius)/(2*self.a*cs.Au2m))
             
-    def isrocky(self): 
-        if self.radius < 1.8: 
-            #rocky planet
-            return True 
-        else: 
-            #enveloped planet
-            return False 
+    def calcMcore(self):
+        if self.isrocky: 
+            self.Mcore = ps.Rcore_to_Mcore(self)
             
+        
             
-star= Star(1, 2, 1, 5700)
-planet1=Planet(1, 365, star, albedo=1)
+    
+class PlanetarySystem(): 
+    
+    def __init__(self, starparams, planetRockyparams, planetEnvparams, Mstar_err=0, Star_age_err=0, Rstar_err=0, Teff_err=0, Rrocky_err=0, Procky_err=0, Xironrocky=1/3, albedo_rocky=0, Renv_err=0, Penv_err=0, Xironenv=1/3, albedo_env=0 ):
+        
+        stardict={'mass_err':Mstar_err, 'age_err':Star_age_err, 'radius_err':Rstar_err, 'Teff_err':Teff_err}
+        self.star=Star(*starparams, **stardict)
+        
+        planetRockydict={'radius_err':Rrocky_err, 'period_err':Procky_err, 'Xiron':Xironrocky, 'albedo':albedo_rocky}
+        self.planetRocky=Planet(*planetRockyparams, isrocky=True, **planetRockydict)
+        self.planetRocky.calcsemimajor(self.star)
+        self.planetRocky.calcplanettemp(self.star)
+        
+        planetEnvdict={'radius_err':Renv_err, 'period_err':Penv_err, 'Xiron':Xironenv, 'albedo':albedo_env}
+        self.planetEnv=Planet(*planetEnvparams, isrocky=False, **planetEnvdict)
+        self.planetEnv.calcsemimajor(self.star)
+        self.planetEnv.calcplanettemp(self.star)
+        
+        
+        
+    
+            
 
 
 
